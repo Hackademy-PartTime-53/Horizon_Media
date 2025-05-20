d2de6600d5372680416b62804fd91eaef6e38b0a
Alessandra Carnevali <alessandracarnevali@Mac.homenet.telecomitalia.it>
2025-05-20 11:58:42 -0700

    modifica nomi in inglese di user sul database

--- database/migrations/2025_05_20_231007_add_role_to_users_table.php
+++ database/migrations/2025_05_20_231007_add_role_to_users_table.php
@@ -12,20 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('età');
-            $table->string('genere');
-            $table->string('stato civile');
+            $table->string('age');
+            $table->string('gender');
+            $table->string('marital status');
             $table->string('taste');
-            $table->string('giornali');
-            $table->string('macchina');
-            $table->string('film&musica');
+            $table->string('newspapers');
+            $table->string('car');
+            $table->string('movie&music');
             $table->string('city');
             $table->string('hobbies');
-            $table->string('attitudini');
-            $table->string('lavoro');
-            $table->boolean('is_admin')->Nullable()->default(false);
-            $table->boolean('is_revisor')->Nullable()->default(false);
-            $table->boolean('is_writer')->Nullable()->default(false);        });
+            $table->string('attitudes');
+            $table->string('job');
+                  });
     }
 
     /**
@@ -34,7 +32,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['età','genere','stato civile','taste','giornali','macchina','film&musica','city','hobbies','attitudini','lavoro']);
+            $table->dropColumn(['age','gender','marital status','taste','newspapers','car','movie&music','city','hobbies','attitudes','job']);
         });
     }
 };
