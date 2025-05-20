dbccf4f55c1c9da25aa8abba7ac7e87ad061b3e4
Alessandra Carnevali <alessandracarnevali@Mac.homenet.telecomitalia.it>
2025-05-20 12:13:00 -0700

    Creata Navbar e footer

--- resources/views/components/shared/footer.blade.php
+++ resources/views/components/shared/footer.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+<footer class="bg-body-tertiary text-center text-lg-start position-fixed w-100 bottom-0">
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+                © 2025 Copyright:
+                <a class="text-body" href="">HorizonMedia</a>
+            </div>
+        </footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-main-layout>
+
+<x-slot:title>Home</x-slot:title>
+
+</x-main-layout>
