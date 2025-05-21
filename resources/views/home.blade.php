771e2e9e062035fa89b47d307e83a00340b2ae5a
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-05-21 12:23:34 +0200

    yes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,12 @@
 
 <x-slot:title>Home</x-slot:title>
 
+<div class="container-fluid p-5 bg-secondary-subtle text-center">
+<div class="row justify-content-center">
+<div class="col-12">
+<h1 class="display-1"> Horizon Media </h1>
+</div>
+</div>
+</div>
+
 </x-main-layout>
