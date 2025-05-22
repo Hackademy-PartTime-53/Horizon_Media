998ef9097fabe9b8019281b15c7294a845f83b9c
Alessandra Carnevali <alessandracarnevali@Mac.homenet.telecomitalia.it>
2025-05-22 12:04:17 -0700

    Modifiche a nav-bar prima del merge

--- resources/views/components/shared/nav-bar.blade.php
+++ resources/views/components/shared/nav-bar.blade.php
@@ -14,7 +14,10 @@
         @guest
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Benvenuto</a>
+            Benvenuto
+          </a>
+          <li>
+            <a class="nav-link active" aria-current="page" href="{{route('article.index')}}">Tutti i nostri articoli</a></li>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
             <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
