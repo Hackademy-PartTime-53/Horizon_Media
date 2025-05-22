da61049529b713800f87bae3f37e86a22594c401
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-05-22 16:28:29 +0200

    User Story 2 completata

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ use App\Http\Controllers\RegisterController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::resource('auth', RegisterController::class)->middleware(['auth']);
-Route::resource('auth', LoginController::class)->middleware(['auth']);
-Route::resource('auth', LogoutController::class)->middleware(['auth']);
+
+// le rotte che gestiscono gli articoli
 Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
 Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
 Route::get('/article/index',[ArticleController::class, 'index'])->name('article.index');
 Route::get('/article/show{article}', [ArticleController::class, 'show'])->name('article.show');
 Route::get('/article/category/{category}', [ArticleController::class, 'byCategory'])->name('article.byCategory');
 Route::get('/article/user/{user}', [ArticleController::class, 'byUser'])->name('article.byUser');
+// le rotte che gestiscono gli articoli
