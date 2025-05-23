48c7a8ad3ee036a256d300d2fb85b4b8c7e7fe5f
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-05-23 14:09:26 +0200

    seed

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -15,11 +15,30 @@ class ArticleSeeder extends Seeder
     {
         Article::create
         ([
-        'title'=> 'impatto dellintelligenza sul lavoro futuro' ,
+        'title'=> 'L\'impatto dell\' Intelligenza sul lavoro futuro' ,
         'subtitle'=> 'Come le tecnologie emergenti stanno trasformando il mondo professionale ',
-        'body'=>'negli ultimi anni , lintelligenza artificiale (ha rivoluzionato diversi settori.)',
+        'body'=>'negli ultimi anni , l\'intelligenza artificiale (ha rivoluzionato diversi settori.)',
         'image'=>"storage/app/public/pexels-cottonbro-8721318.jpg",
         'category_id'=>6,
+
+    ]);
+
+     Article::create
+        ([
+        'title'=> 'Olimpiadi Milano Cortina 2026 - caos trasporti' ,
+        'subtitle'=> 'Grandi problemi sui trasporti',
+        'body'=>'Purtroppo sulle olimpiadi non abbiamo molte cose da dire. Il sistema di territorio si è mosso con grave ritardo, solo all\'ultimo anno, il tavolo sulla mobilità si sta muovendo. Parole importanti pronunciate da Gabriele Mariani. ',
+        'image'=>"storage/app/public/pexels.jpg",
+        'category_id'=>4,
+
+    ]);
+     Article::create
+        ([
+        'title'=> 'Avengers' ,
+        'subtitle'=> 'Avangers: Doomsday e Secret Wars posticipano la data di uscita al cinema',
+        'body'=>'Disney sta apportando delle modifiche al suo calendario di uscita, posticipando entrambi i progetti Marvel',
+        'image'=>"storage/app/public/pexels-cottonbro-8721318.jpg",
+        'category_id'=>5,
 
     ]);
 }
