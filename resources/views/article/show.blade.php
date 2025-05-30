13425803dcaeca3f0ce35edfa51f78e481c5bc95
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-05-30 15:00:09 +0200

    save

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid p-5 bg-secondary-subtle text-center">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h1 class="display-1">{{ $article->title }}</h1>
+                <h1 class="display-1 page-article-title">{{ $article->title }}</h1>
             </div>
         </div>
     </div>
@@ -14,42 +14,58 @@
             <div class="col-12 col-md-8 d-flex flex-column">
                 <img src="{{ Storage::url($article->image) }}" class="img-fluid"
                     alt="Immagine dell'articolo: {{ $article->title }}">
+
                 <div class="text-center">
-                    <h2>{{ $article->subtitle }}</h2>
-                    <p class="fs-5">Categoria:
-                        <a href="{{ route('article.byCategory', $article->category) }}" class="text-capitalize fw-bold text-muted">{{ $article->category->name }}</a>
+                    <h2 class="display-1 article-title">{{ $article->subtitle }}</h2>
+                </div>
+
+                <div class="text-center d-flex justify-content-center gap-4 my-3 flex-wrap">
+                    <p class="fs-5 mb-0">
+                        Categoria: 
+                        <a href="{{ route('article.byCategory', $article->category) }}" class="text-capitalize fw-bold text-muted">
+                            {{ $article->category->name }}
+                        </a>
                     </p>
-                    <p class="fs-5">Redattore:
-                        <a href="{{ route('article.byUser', $article->user) }}" class="text-capitalize fw-bold text-muted">{{ $article->user->name }}</a>
+
+                    <p class="fs-5 mb-0">
+                        Redattore: 
+                        <a href="{{ route('article.byUser', $article->user) }}" class="text-capitalize fw-bold text-muted">
+                            {{ $article->user->name }}
+                        </a>
+                    </p>
+
+                    <p class="fs-5 mb-0 text-muted">
+                        Redatto il {{ $article->created_at->format('d/m/Y') }}
                     </p>
                 </div>
-                <div class="text-muted my-3">
-                    <p>Redatto il {{ $article->created_at->format('d/m/Y') }} da {{ $article->user->name }}</p>
-                </div>
+
                 <hr>
-                <p>{{ $article->body }}</p>
+
+                <p class="article-body">{{ $article->body }}</p>
+
                 @if (Auth::user() && Auth::user()->is_revisor)
                     <div class="container my-5">
                         <div class="row">
-                        <div class="col-12 d-flex justify-content-evenly">
-                            <form action="{{ route('revisor.acceptArticle', $article) }}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn btn-success">Accetta l'articolo</button>
-                            </form>
+                            <div class="col-12 d-flex justify-content-evenly">
+                                <form action="{{ route('revisor.acceptArticle', $article) }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success">Accetta l'articolo</button>
+                                </form>
 
-                            <form action="{{ route('revisor.rejectArticle', $article) }}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn btn-danger">Rifiuta l'articolo</button>
-                            </form>
-                        </div>
+                                <form action="{{ route('revisor.rejectArticle', $article) }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Rifiuta l'articolo</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    @endif
+                @endif
+
                 <div class="text-center">
                     <a href="{{ route('article.index') }}" class="text-secondary">Vai alla lista degli articoli</a>
                 </div>
             </div>
         </div>
     </div>
-
-    </x-main-layout>
+ <div class="bgcolor" style="padding-bottom: 500px;">
+</x-main-layout>
