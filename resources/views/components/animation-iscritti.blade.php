7dba9339a9b8a4161c8822c1e8a6b7471c3078b4
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-05-30 13:26:16 +0200

    save

--- resources/views/components/animation-iscritti.blade.php
+++ resources/views/components/animation-iscritti.blade.php
@@ -1,10 +1,16 @@
-<div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
+<div class="container-fluid min-vh-100 d-flex justify-content-center align-items-center bg-light">
   <div id="nostri_numeri" class="text-center">
-    <h2 class="titolo_ani">I NOSTRI NUMERI:</h2>
-    <p><span id="number1">0</span> Articoli</p>
-    <p><span id="number2">0</span> Iscritti</p>
-    <p><span id="number3">0</span> Abbonati</p>
+    <h2 class="titolo_ani mb-4">I NOSTRI NUMERI:</h2>
+    <div class="row">
+      <div class="col-12 col-md-4 mb-3">
+        <p class="fs-4"><span id="number1" class="counter" data-target="1000">0</span><br><small>Articoli</small></p>
+      </div>
+      <div class="col-12 col-md-4 mb-3">
+        <p class="fs-4"><span id="number2" class="counter" data-target="5000">0</span><br><small>Iscritti</small></p>
+      </div>
+      <div class="col-12 col-md-4 mb-3">
+        <p class="fs-4"><span id="number3" class="counter" data-target="1200">0</span><br><small>Abbonati</small></p>
+      </div>
+    </div>
   </div>
 </div>
-
-
